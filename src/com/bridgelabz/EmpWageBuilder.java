a1253664626ff46a54803df4a135399f02feef02
jhanvi006 <jhanvikankhara06@gmail.com>
2022-04-23 06:53:58 -0400

    Added: daily wage calculation logic

--- src/com/bridgelabz/EmpWageBuilder.java
+++ src/com/bridgelabz/EmpWageBuilder.java
@@ -5,15 +5,18 @@ import java.util.Random;
 public class EmpWageBuilder {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program.");
-
+        int WAGE_PR_HOUR = 20;
+        int FULL_DAY_HOUR = 8;
 //      Generate random number to check emp presence
         Random random = new Random();
         int isPresent = random.nextInt(2);
-
+        int workingHour, dailyWage;
 //      Check Presence
         if (isPresent == 1)
-            System.out.println("Employee is present.");
+            workingHour = FULL_DAY_HOUR;
         else
-            System.out.println("Employee is absent.");
+            workingHour = 0;
+        dailyWage = workingHour*WAGE_PR_HOUR;
+        System.out.println("Daily wage is "+dailyWage);
     }
 }

4b7d7ff23a5f1edad6ac05b91cdff26082a882bf
jhanvi006 <jhanvikankhara06@gmail.com>
2022-04-23 07:31:12 -0400

    Added: monthly wage calculation logic

--- src/com/bridgelabz/EmpWageBuilder.java
+++ src/com/bridgelabz/EmpWageBuilder.java
@@ -6,29 +6,36 @@ public class EmpWageBuilder {
     public static final int WAGE_PR_HOUR = 20;
     public static final int FULL_DAY_HOUR = 8;
     public static final int HALF_DAY_HOUR = 4;
+    public static final int WORKING_DAY_PR_MONTH = 20;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program.");
 
-//      Generate random number to check emp presence
+        /* Generate random no. to check emp presence */
         Random random = new Random();
         final int IS_PART_TIME = 1;
         final int IS_FULL_TIME = 2;
-        int checkPresence = random.nextInt(3);
-        int workingHour, dailyWage;
+        int startDay = 1;
+        int workingHour=0, wage=0, checkPresence;
 
-//      Check Presence
-        switch (checkPresence){
-            case IS_PART_TIME:
-                workingHour = HALF_DAY_HOUR;
-                break;
-            case IS_FULL_TIME:
-                workingHour = FULL_DAY_HOUR;
-                break;
-            default: workingHour = 0;
+        /* Calculate for whole month */
+        while (startDay <= WORKING_DAY_PR_MONTH) {
+            checkPresence = random.nextInt(3);
+            /* Check Presence */
+            switch (checkPresence) {
+                case IS_PART_TIME:
+                    workingHour = HALF_DAY_HOUR;
+                    break;
+                case IS_FULL_TIME:
+                    workingHour = FULL_DAY_HOUR;
+                    break;
+                default:
+                    workingHour = 0;
+            }
+
+            wage += workingHour * WAGE_PR_HOUR;
+            startDay++;
         }
-
-        dailyWage = workingHour*WAGE_PR_HOUR; /* Possible outputs 0, 80, 160 */
-        System.out.println("Daily wage is "+dailyWage);
+        System.out.println("Monthly wage is " + wage);
     }
 }

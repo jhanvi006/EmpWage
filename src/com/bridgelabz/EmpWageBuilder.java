548814f6608d8bdfe931ced7213d52feda9e0b4c
jhanvi006 <jhanvikankhara06@gmail.com>
2022-04-23 06:39:34 -0400

    Added: Employee attendance logic

--- src/com/bridgelabz/EmpWageBuilder.java
+++ src/com/bridgelabz/EmpWageBuilder.java
@@ -1,7 +1,19 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class EmpWageBuilder {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program.");
+
+//      Generate random number to check emp presence
+        Random random = new Random();
+        int isPresent = random.nextInt(2);
+
+//      Check Presence
+        if (isPresent == 1)
+            System.out.println("Employee is present.");
+        else
+            System.out.println("Employee is absent.");
     }
 }

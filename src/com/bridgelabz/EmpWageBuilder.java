929d45373dd10f633cc993ae8a2e5fc8a7d31bd4
jhanvi006 <jhanvikankhara06@gmail.com>
2022-04-26 16:59:40 -0400

    Modified: added class and methods

--- src/com/bridgelabz/EmpWageBuilder.java
+++ src/com/bridgelabz/EmpWageBuilder.java
@@ -1,23 +1,17 @@
 package com.bridgelabz;
-
 import java.util.Random;
 
-public class EmpWageBuilder {
-    public static final int WAGE_PR_HOUR = 20;
-    public static final int FULL_DAY_HOUR = 8;
-    public static final int HALF_DAY_HOUR = 4;
-    public static final int WORKING_DAY_PR_MONTH = 20;
-
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program.");
-
-        /* Generate random no. to check emp presence */
-        Random random = new Random();
-        final int IS_PART_TIME = 1;
-        final int IS_FULL_TIME = 2;
-        int startDay = 1;
-        int workingHour=0, wage=0, checkPresence, totalWorkingHr=0;
-
+class WageCalculation{
+    Random random = new Random();
+    public final int WAGE_PR_HOUR = 20;
+    public final int FULL_DAY_HOUR = 8;
+    public final int HALF_DAY_HOUR = 4;
+    public final int WORKING_DAY_PR_MONTH = 20;
+    public final int IS_PART_TIME = 1;
+    public final int IS_FULL_TIME = 2;
+    int startDay = 1;
+    int workingHour=0, wage=0, checkPresence, totalWorkingHr=0;
+    public void calculateMonthlyWage(){
         /* Calculate for whole month */
         while (startDay <= WORKING_DAY_PR_MONTH && totalWorkingHr<=100) {
             checkPresence = random.nextInt(3);
@@ -32,12 +26,19 @@ public class EmpWageBuilder {
                 default:
                     workingHour = 0;
             }
-
             wage += workingHour * WAGE_PR_HOUR;
             totalWorkingHr+=workingHour;
             startDay++;
         }
         System.out.println("Working hours: "+totalWorkingHr);
         System.out.println("Monthly wage is " + wage);
+    }
+}
+public class EmpWageBuilder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program.");
+
+        WageCalculation wc = new WageCalculation();
+        wc.calculateMonthlyWage();
     }
 }

c7f8d97f6733fb0507d4c9a6c9565b71fd99aaa2
jhanvi006 <jhanvikankhara06@gmail.com>
2022-04-26 17:17:18 -0400

    Added: added various objects with required arguments for multiple company wage calculation

--- src/com/bridgelabz/EmpWageBuilder.java
+++ src/com/bridgelabz/EmpWageBuilder.java
@@ -3,17 +3,15 @@ import java.util.Random;
 
 class WageCalculation{
     Random random = new Random();
-    public final int WAGE_PR_HOUR = 20;
     public final int FULL_DAY_HOUR = 8;
     public final int HALF_DAY_HOUR = 4;
-    public final int WORKING_DAY_PR_MONTH = 20;
     public final int IS_PART_TIME = 1;
     public final int IS_FULL_TIME = 2;
     int startDay = 1;
     int workingHour=0, wage=0, checkPresence, totalWorkingHr=0;
-    public void calculateMonthlyWage(){
+    public void calculateMonthlyWage(int wagePrHour, int workingDays, int workingHrsPrMonth, String companyName){
         /* Calculate for whole month */
-        while (startDay <= WORKING_DAY_PR_MONTH && totalWorkingHr<=100) {
+        while (startDay <= workingDays && totalWorkingHr<=workingHrsPrMonth) {
             checkPresence = random.nextInt(3);
             /* Check Presence */
             switch (checkPresence) {
@@ -26,19 +24,21 @@ class WageCalculation{
                 default:
                     workingHour = 0;
             }
-            wage += workingHour * WAGE_PR_HOUR;
+            wage += workingHour * wagePrHour;
             totalWorkingHr+=workingHour;
             startDay++;
         }
-        System.out.println("Working hours: "+totalWorkingHr);
-        System.out.println("Monthly wage is " + wage);
+        System.out.println("Total working hours for "+companyName+" company: "+totalWorkingHr);
+        System.out.println("Monthly wage of employee at "+companyName+" company is " + wage);
     }
 }
 public class EmpWageBuilder {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program.");
 
-        WageCalculation wc = new WageCalculation();
-        wc.calculateMonthlyWage();
+        WageCalculation tcs = new WageCalculation();
+        WageCalculation infosys = new WageCalculation();
+        tcs.calculateMonthlyWage(30, 25, 120, "tcs");
+        infosys.calculateMonthlyWage(25, 18, 150, "infosys");
     }
 }
